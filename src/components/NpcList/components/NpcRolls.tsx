db7acb7a06fc8a62f28d8d96aced0c2f0a5f4d74
Anborgh <divoannet@gmail.com>
2025-06-30 15:07:16 +0300

    add custom label value

--- src/components/NpcList/components/NpcRolls.tsx
+++ src/components/NpcList/components/NpcRolls.tsx
@@ -7,38 +7,46 @@ export const NpcRolls = () => {
   const rolls = useNpcStore(state => state.rolls);
   const setRoll = useNpcStore(state => state.setRoll);
   const customRollTitle = useNpcStore(state => state.customRollTitle);
+  const setCustomRollTitle = useNpcStore(state => state.setCustomRollTitle);
 
   return (
     <Box>
       <RollItem
-        label='Боевые:'
+        label='Боевые'
         value={rolls.battle}
         onChange={(newValue) => setRoll('battle', newValue)}
+        copy
       />
       <RollItem
-        label='Интеллект:'
+        label='Интеллект'
         value={rolls.intellect}
         onChange={(newValue) => setRoll('intellect', newValue)}
+        copy
       />
       <RollItem
-        label='Технические:'
+        label='Технические'
         value={rolls.craft}
         onChange={(newValue) => setRoll('craft', newValue)}
+        copy
       />
       <RollItem
-        label='Физ. сопротивление:'
+        label='Физ. сопротивление'
         value={rolls.physical}
         onChange={(newValue) => setRoll('physical', newValue)}
+        copy
       />
       <RollItem
-        label='Социальные:'
+        label='Социальные'
         value={rolls.social}
         onChange={(newValue) => setRoll('social', newValue)}
+        copy
       />
       <RollItem
-        label={`${customRollTitle}:`}
+        label={`${customRollTitle}`}
         value={rolls.custom}
         onChange={(newValue) => setRoll('custom', newValue)}
+        onLabelChange={setCustomRollTitle}
+        copy
       />
     </Box>
   )
--- src/components/NpcList/components/RollItem.tsx
+++ src/components/NpcList/components/RollItem.tsx
@@ -1,32 +1,78 @@
-import { Box, HStack, IconButton, NumberInput } from "@chakra-ui/react"
-import { LuCopy } from "react-icons/lu"
+import { Box, Field, Group, HStack, IconButton, Input, NumberInput, Popover } from "@chakra-ui/react"
+import { LuCheck, LuCopy, LuPencilLine } from "react-icons/lu"
 import { useClipboard } from "@/hooks/useClipboard";
+import { useEffect, useState, type ChangeEvent } from "react";
 
 interface Props {
   label: string;
   value: number;
   onChange: (newValue: number) => void;
+  onLabelChange?: (newValue: string) => void;
+  copy?: boolean;
 }
 
 export const RollItem = ({
   label,
   value,
-  onChange
+  onChange,
+  onLabelChange,
+  copy,
 }: Props) => {
   const { copyToClipboard } = useClipboard();
+  const [localLabel, setLocalLabel] = useState('')
+  const [open, setOpen] = useState(false)
+
+  const handleSaveLabel = () => {
+    onLabelChange && onLabelChange(localLabel);
+    setOpen(false);
+  }
+
+  const handleLabelChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setLocalLabel(event.target.value)
+  }
+
+  useEffect(() => {
+    setLocalLabel(label)
+  }, [label]);
 
   return (
     <HStack padding={2}>
-      <Box width="180px">
+      <Box minW="180px">
         {label}
+        {Boolean(onLabelChange) && (
+          <Popover.Root positioning={{ placement: 'top' }} open={open} onOpenChange={(e) => setOpen(e.open)}>
+            <Popover.Trigger>
+              <IconButton size='xs' variant='ghost'>
+                <LuPencilLine />
+              </IconButton>
+            </Popover.Trigger>
+            <Popover.Positioner>
+              <Popover.Content>
+                <Popover.CloseTrigger />
+                <Popover.Arrow />
+                <Popover.Body>
+                  <Field.Root required>
+                    <Field.Label>Название кастомной проверки</Field.Label>
+                    <Group gap="xs">
+                      <Input placeholder="Кастомная проверка" value={localLabel} onChange={handleLabelChange} />
+                      <IconButton onClick={handleSaveLabel}><LuCheck /></IconButton>
+                    </Group>
+                  </Field.Root>
+                </Popover.Body>
+              </Popover.Content>
+            </Popover.Positioner>
+          </Popover.Root>
+        )}
       </Box>
       <NumberInput.Root value={`${value}`} onValueChange={(e) => onChange(Number(e.value) || 0)} width="80px">
         <NumberInput.Control />
         <NumberInput.Input />
       </NumberInput.Root>
-      <IconButton variant="ghost" size="xs" onClick={() => copyToClipboard(`/roll ${value}d6>4`)}>
-        <LuCopy />
-      </IconButton>
+      {copy && (
+        <IconButton variant="ghost" size="xs" onClick={() => copyToClipboard(`/roll ${value}d6>4`)}>
+          <LuCopy />
+        </IconButton>
+      )}
     </HStack>
   )
 }
